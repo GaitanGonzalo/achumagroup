c0de4020a6c416a6ecff83064bd5924aabc5f5e7
Gaitan Gonzalo <gaitan.jorgegonzalo959@gmail.com>
2024-09-11 18:12:58 -0300

    first commit construction page

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import ConstructionPage from './pages/contruction/Construction';
+function App() {
+  return (
+    <div className="App">
+      <ConstructionPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/contruction/Construction.js
@@ -0,0 +1,13 @@
+import logo from '../../assets/Logo_verde.png'; 
+import './Construction.css'
+function ConstructionPage(){
+    return (
+        <body className="App-body">
+            <img src={logo} className="Construction-logo" alt="logo" />
+            <h2>PÁGINA EN CONSTRUCCIÓN</h2>
+        </body>
+
+    )
+}
+
+export default ConstructionPage;
